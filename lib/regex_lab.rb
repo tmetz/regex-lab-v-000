dcbbf09f840f8db0af7e873b9e1571fa14114216
Tammy Metz <tammy@tammymetz.com>
2018-07-10 08:12:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b[A-Z]\w*/) ? true : false && text =~ /[!?.]$/
+  first_word = text.match(/\b[A-Z]\w*/) ? true : false
+  punctuation = text =~ /[!?.]$/
+  return first_word && punctuation
 end
 
 def valid_phone_number?(phone)
