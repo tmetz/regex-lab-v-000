524fc584884a2e9bfecfa34be819bb41a008a1ea
Tammy Metz <tammy@tammymetz.com>
2018-07-10 08:25:03 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  word[0] =~ /[aeiou]/i ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  first_word = text.match(/^\b[A-Z]\w*/) ? true : false
+  punctuation = text.match(/[!?\.]$/) ? true : false
+  return first_word && punctuation
 end
 
 def valid_phone_number?(phone)
-
+  phone =~ /\D*(\d{3})\D*(\d{3})\D*(\d{4})/
 end
